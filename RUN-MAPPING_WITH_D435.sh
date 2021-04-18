a3e28133ed968774ea2c0dfa2cc8e3c4cb21cfda
Tossy <syun111@gmail.com>
2021-04-18 22:31:17 +0900

    <add> record ok

--- RUN-MAPPING_WITH_D435.sh
+++ RUN-MAPPING_WITH_D435.sh
@@ -3,8 +3,21 @@
 # Load setup.bash
 source ~/.bashrc
 
-now_date=`date '+%Y%m%d-%H:%M:%S'`
+# 
+readonly SETTING_FILE_PATH="Examples/RGB-D/D435.yaml"
+echo "Setting file path            >>  ${SETTING_FILE_PATH}"
 
-readonly SETTING_FILE_PATH=`Examples/RGB-D/D435.yaml`
-echo $SETTING_FILE_PATH
-# rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.txt Examples/RGB-D/D435.yaml /camera/rgb/image_raw:=/camera/color/image_raw /camera/depth_registered/image_raw:=/camera/aligned_depth_to_color/image_raw
+readonly SUBSCRIBE_IMAGE_COLOR="/camera/rgb/image_raw:=/camera/color/image_raw"
+echo "Subscribe image color topic  >>  ${SUBSCRIBE_IMAGE_COLOR}"
+
+readonly SUBSCRIBE_IMAGE_DEPTH="/camera/depth_registered/image_raw:=/camera/aligned_depth_to_color/image_raw"
+echo "Subscribe image depth topic  >>  ${SUBSCRIBE_IMAGE_DEPTH}"
+
+# 実行コマンド作成
+_cmd="rosrun ORB_SLAM2 RGBD Vocabulary/ORBvoc.txt ${SETTING_FILE_PATH} ${SUBSCRIBE_IMAGE_COLOR} ${SUBSCRIBE_IMAGE_DEPTH}"
+echo "実行コマンド
+${_cmd}"
+
+# 実行
+$_cmd
+
--- RUN-MAPPING_WITH_ZED.sh
+++ RUN-MAPPING_WITH_ZED.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-rosrun ORB_SLAM2 Stereo Vocabulary/ORBvoc.txt Examples/Stereo/ZED.yaml true /camera/left/image_raw:=/zed/zed_node/left/image_rect_gray /camera/right/image_raw:=/zed/zed_node/right/image_rect_gray
+# rosrun ORB_SLAM2 Stereo Vocabulary/ORBvoc.txt Examples/Stereo/ZED.yaml true /camera/left/image_raw:=/zed/zed_node/left/image_rect_gray /camera/right/image_raw:=/zed/zed_node/right/image_rect_gray
+
+
+# Load setup.bash
+source ~/.bashrc
+
+# 
+readonly SETTING_FILE_PATH="Examples/Stereo/ZED.yaml"
+echo "Setting file path            >>  ${SETTING_FILE_PATH}"
+
+readonly SUBSCRIBE_IMAGE_RIGHT="/camera/right/image_raw:=/zed/zed_node/right/image_rect_gray"
+echo "Subscribe image right topic  >>  ${SUBSCRIBE_IMAGE_RIGHT}"
+
+readonly SUBSCRIBE_IMAGE_LEFT="/camera/left/image_raw:=/zed/zed_node/left/image_rect_gray"
+echo "Subscribe image left topic  >>  ${SUBSCRIBE_IMAGE_LEFT}"
+
+
+# 実行コマンド作成
+_cmd="rosrun ORB_SLAM2 Stereo Vocabulary/ORBvoc.txt ${SETTING_FILE_PATH} true ${SUBSCRIBE_IMAGE_RIGHT} ${SUBSCRIBE_IMAGE_LEFT}"
+echo "実行コマンド
+${_cmd}"
+
+# 実行
+$_cmd
