2de6b20e15620b99d3900c9555a36641a465d35f
Tossy <syun111@gmail.com>
2021-04-18 17:51:54 +0900

    <fix> recode file path

--- RUN-ROSBAG_RECORD.sh
+++ RUN-ROSBAG_RECORD.sh
@@ -2,4 +2,4 @@
 source ~/.bashrc
 now_date=`date '+%Y%m%d-%H:%M:%S'`
 echo $now_date
-rosbag record /camera/aligned_depth_to_color/image_raw /camera/color/image_raw -O $now_date
+rosbag record /camera/aligned_depth_to_color/image_raw /camera/color/image_raw -O ./bagdata/$now_date
