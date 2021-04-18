aa4c2d357af67fd5e45ca95f19d9b3d9bba0e567
Tossy <syun111@gmail.com>
2021-04-18 22:32:24 +0900

    <add> bag record scripts, record ok

--- RUN-ROSBAG_RECORD.sh
+++ RUN-ROSBAG_RECORD.sh
@@ -1,5 +1,98 @@
 #!/bin/bash
-source ~/.bashrc
-now_date=`date '+%Y%m%d-%H:%M:%S'`
-echo $now_date
-rosbag record /camera/aligned_depth_to_color/image_raw /camera/color/image_raw -O ./bagdata/$now_date
+
+
+function checkdir()
+{   
+
+    # 任意のディレクトリが存在するか確認
+    # echo "args : $1"
+    
+    # 実行コマンドを作成
+    _cmd="${1}"
+
+    # dirがないときに処理するところ
+    if [ ! -d $1 ]; then
+        echo "bagdataを保存するディレクトリが見つかりませんでした."
+        echo "保存ディレクトリを作成します. ($_cmd)"
+        mkdir $_cmd
+    fi
+}
+
+
+# ============================================================================ # 
+
+
+# echo "実行時の引数の数 >> ${#}"
+
+# 保存するディレクトリパス
+SAVE_DIRPATH=$PWD
+
+# 保存するディレクトリ名
+SAVE_DIRNAME="bagdata2"
+
+# bagファイルを保存するファイルパス
+SAVE_FILEPATH="${SAVE_DIRPATH}/${SAVE_DIRNAME}"
+echo "$SAVE_FILEPATH"
+
+# bagdataデータ名とする実行時刻を取得
+NOW_DATE=`date '+%Y%m%d-%H%M%S'`
+
+
+
+# 引数が指定されていない or "-h"が渡された時
+if [ $# = 0 ] || [ $1 = "-h" ]; then
+    
+    echo "引数を指定してください."
+    echo "ZED Camera    : $ bash RUN-ROSBAG_RECORD.sh zed"
+    echo "RealSenseD435 : $ bash RUN-ROSBAG_RECORD.sh rs"
+
+
+
+# 引数がzedの場合
+elif [ $1 = "zed" ]; then
+    
+    echo "ZED Camera"
+
+    # 保存ディレクトリがあるかを確認
+    checkdir $SAVE_FILEPATH
+
+    echo "保存するディレクトリパス: 
+$SAVE_DIRPATH"
+
+    echo "保存するファイル名: 
+${NOW_DATE}_zed.bag"
+
+    # 実行コマンド作成
+    _cmd="rosbag record /zed/zed_node/right/image_rect_gray /zed/zed_node/left/image_rect_gray -O ${SAVE_FILEPATH}/${NOW_DATE}_zed"
+    echo "実行コマンド:
+$_cmd"
+    
+    # 実行 
+    source ~/.bashrc
+    $_cmd
+
+
+
+# 引数がrsの場合
+elif [ $1 = "rs" ]; then
+
+    echo "rs"
+    
+    # 保存ディレクトリがあるかを確認
+    checkdir $SAVE_FILEPATH
+    
+    echo "保存するディレクトリパス: 
+$SAVE_DIRPATH"
+
+    echo "保存するファイル名: 
+${NOW_DATE}_rs.bag"
+
+    # 実行コマンド作成
+    _cmd="rosbag record /camera/aligned_depth_to_color/image_raw /camera/color/image_raw -O ${SAVE_FILEPATH}/${NOW_DATE}_rs"
+    echo "実行コマンド:
+$_cmd"
+    
+    # 実行 
+    source ~/.bashrc
+    $_cmd 
+fi
